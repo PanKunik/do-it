9d3466e179a8ca3bb4afd4272bd75f8499bdd784
PanKunik <patryk.kunicki@pm.me>
2026-01-05 13:41:47 +0100

    Feature #9 - EntityId<T> unit tests

--- backend/tests/DoIt.Domain.Unit.Tests/Shared/EntityIdTests.cs
+++ backend/tests/DoIt.Domain.Unit.Tests/Shared/EntityIdTests.cs
@@ -5,6 +5,89 @@ namespace DoIt.Domain.Unit.Tests.Shared;
 public class EntityIdTests
 {
     [Fact]
+    public async Task EntityId_WhenCalledWithNullValue_ShouldThrowArgumentNullExcetpion()
+    {
+        // Arrange
+        var createEntityId = () => new StringId(null);
+
+        // Act && Assert
+        Assert.Throws<ArgumentNullException>(createEntityId);
+
+        await Task.CompletedTask;
+    }
+
+    [Fact]
+    public async Task ToString_WhenCalled_ShouldReturnExpectedString()
+    {
+        // Arrange
+        var cut = new IntId(2);
+
+        // Act
+        var result = cut.ToString();
+
+        // Assert
+        Assert.Equal("2", result);
+
+        await Task.CompletedTask;
+    }
+
+    [Theory]
+    [InlineData("00000000-0000-0000-0000-000000000001", "00000000-0000-0000-0000-000000000001")]
+    [InlineData("A0032000-00C0-0080-0000-00F020000101", "A0032000-00C0-0080-0000-00F020000101")]
+    public async Task GetHashCode_WhenCalledForSameValues_ShouldBeEqual(string value, string otherValue)
+    {
+        // Arrange
+        var cut = new ActivityId(Guid.Parse(value));
+        var other = new ActivityId(Guid.Parse(otherValue));
+
+        // Act
+        var cutHashCode = cut.GetHashCode();
+        var otherHashCode = other.GetHashCode();
+
+        // Assert
+        Assert.Equal(cutHashCode, otherHashCode);
+
+        await Task.CompletedTask;
+    }
+
+    [Theory]
+    [InlineData("A0032000-00C0-0080-0000-00F020000101", "00000000-0000-0000-0000-000000000001")]
+    [InlineData("00000000-0000-0000-0000-000000000001", "A0032000-00C0-0080-0000-00F020000101")]
+    public async Task GetHashCode_WhenCalledForOtherValues_ShouldNotBeEqual(string value, string otherValue)
+    {
+        // Arrange
+        var cut = new ActivityId(Guid.Parse(value));
+        var other = new ActivityId(Guid.Parse(otherValue));
+
+        // Act
+        var cutHashCode = cut.GetHashCode();
+        var otherHashCode = other.GetHashCode();
+
+        // Assert
+        Assert.NotEqual(cutHashCode, otherHashCode);
+
+        await Task.CompletedTask;
+    }
+
+    [Fact]
+    public async Task Equals_WhenOtherIsNotValueObject_ShouldReturnFalse()
+    {
+        // Arrange
+        var guid = Guid.NewGuid();
+        var cut = new UserId(guid);
+        var otherValue = guid;
+
+        // Act
+        var result = cut.Equals(otherValue);
+
+        // Assert
+        Assert.False(result);
+
+        await Task.CompletedTask;
+    }
+
+
+    [Fact]
     public async Task Equals_WhenOtherHasSameValue_ShouldReturnTrue()
     {
         // Arrange
@@ -159,4 +242,10 @@ public class EntityIdTests
     
     private sealed class ActivityId(Guid value)
         : EntityId<Guid>(value);
+
+    private sealed class StringId(string value)
+        : EntityId<string>(value);
+
+    private sealed class IntId(int value)
+        : EntityId<int>(value);
 }
